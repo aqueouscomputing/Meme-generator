2c65cc1d3d465319545e0769d28c794bddb2fab6
aqueouscomputing <aqueouscomputing@gmail.com>
2016-06-02 21:45:28 -0700

    Add files via upload

---
+++ meme.sh
@@ -0,0 +1,29 @@
+echo -e "Welcome to the meme generator nigga\n"
+echo -e ""
+echo -e "What meme can I give you today?\n"
+echo -e ""
+echo -e "1.) Navy Seal\n"
+echo -e "2.) Daddy's cummies\n"
+echo -e "3.) Gawd caught me hotboxin\n"
+
+echo -e "Meme: \c"
+
+read MEME
+
+if [ $MEME -eq "1" ]
+then
+echo -e "What the fuck did you just fucking say about me, you little bitch? I’ll have you know I graduated top of my class in the Navy Seals, and I’ve been involved in numerous secret raids on Al-Quaeda, and I have over 300 confirmed kills. I am trained in gorilla warfare and I’m the top sniper in the entire US armed forces. You are nothing to me but just another target. I will wipe you the fuck out with precision the likes of which has never been seen before on this Earth, mark my fucking words. You think you can get away with saying that shit to me over the Internet? Think again, fucker. As we speak I am contacting my secret network of spies across the USA and your IP is being traced right now so you better prepare for the storm, maggot. The storm that wipes out the pathetic little thing you call your life. You’re fucking dead, kid. I can be anywhere, anytime, and I can kill you in over seven hundred ways, and that’s just with my bare hands. Not only am I extensively trained in unarmed combat, but I have access to the entire arsenal of the United States Marine Corps and I will use it to its full extent to wipe your miserable ass off the face of the continent, you little shit. If only you could have known what unholy retribution your little “clever” comment was about to bring down upon you, maybe you would have held your fucking tongue. But you couldn’t, you didn’t, and now you’re paying the price, you goddamn idiot. I will shit fury all over you and you will drown in it. You’re fucking dead, kiddo."
+exit
+fi
+
+if [ $MEME -eq "2" ]
+then
+echo -e "IM DELETING YOU, DADDY!😭👋 ██]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]] 10% complete..... ████]]]]]]]]]]]]]]]]]]]]]]]]]]] 35% complete.... ███████]]]]]]]]]]]]]]]] 60% complete.... ███████████] 99% complete..... 🚫ERROR!🚫 💯True💯 Daddies are irreplaceable 💖I could never delete you Daddy!💖 Send this to ten other 👪Daddies👪 who give you 💦cummies💦 Or never get called ☁️squishy☁️ again❌❌😬😬❌❌ If you get 0 Back: no cummies for you 🚫🚫👿 3 back: you're squishy☁️💦 5 back: you're daddy's kitten😽👼💦 10+ back: Daddy😛😛💕💕💦👅👅"
+exit
+fi
+
+if [ $MEME -eq "3" ]
+then
+echo -e "I’m not allowed back in church after god caught me hotboxing the confessional booth  I told him “god, if you don’t want me smoking weed, then why the hell did you make this shit so dank???” god paused for a sec, looked me dead in the eye, and said “my son, im not mad at you for blazing it…I’m mad at you for not passing that shit!!” lololol  send 2 420 ppl or U will never smoke kush again !!!!!!!!!!!!"
+exit
+fi
